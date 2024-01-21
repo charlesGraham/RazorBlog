0ca6e88a7238a1a6a9d5873db5d5fdbb43198883
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2024-01-21 16:23:34 -0600

    Added Sql Server packages

--- Models/Domain/Tag.cs
+++ Models/Domain/Tag.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace RazorBlog.Models.Domain
 {
     public class Tag
     {
-        
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Guid BlogPostId { get; set; }
     }
 }
