210f9d4adc5dfa4703b8aec7a3097c6cbf365c84
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2024-01-21 16:18:04 -0600

    first commit

---
+++ Models/Domain/Tag.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorBlog.Models.Domain
+{
+    public class Tag
+    {
+        
+    }
+}
