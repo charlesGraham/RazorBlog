60b48c99e806836fb6a96b2c8e06990a9610eb49
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2024-01-21 18:35:58 -0600

    Added functionality to create blog post

--- Pages/Admin/Blogs/Add.cshtml.cs
+++ Pages/Admin/Blogs/Add.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorBlog.Data;
+using RazorBlog.Models.Domain;
 using RazorBlog.Models.ViewModels;
 
 namespace RazorBlog.Pages.Admin.Blogs
@@ -7,16 +9,38 @@ namespace RazorBlog.Pages.Admin.Blogs
     public class Add : PageModel
     {
         [BindProperty]
-        public AddBlogPost AddBlogPostReq { get; set; }
+        public AddBlogPost? AddBlogPostReq { get; set; }
+        private readonly RazorBlogDbContext _razorBlogDbContext;
+
+        public Add(RazorBlogDbContext razorBlogDbContext)
+        {
+            _razorBlogDbContext = razorBlogDbContext;
+        }
 
         public void OnGet() { }
 
         public void OnPost()
         {
-            var heading = Request.Form["heading"];
-            var title = Request.Form["title"];
-            var content = Request.Form["content"];
-            var shortDescription = Request.Form["shortDescription"];
+            var blogPost = new BlogPost();
+
+            if (AddBlogPostReq is not null)
+            {
+                blogPost = new BlogPost()
+                {
+                    Heading = AddBlogPostReq.Heading,
+                    PageTitle = AddBlogPostReq.PageTitle,
+                    Content = AddBlogPostReq.Content,
+                    ShortDescription = AddBlogPostReq.ShortDescription,
+                    FeaturedImageUrl = AddBlogPostReq.FeaturedImageUrl,
+                    UrlHandle = AddBlogPostReq.UrlHandle,
+                    PublishedDate = AddBlogPostReq.PublishedDate,
+                    Author = AddBlogPostReq.Author,
+                    Visible = AddBlogPostReq.Visible,
+                };
+            }
+
+            _razorBlogDbContext.BlogPosts.Add(blogPost);
+            _razorBlogDbContext.SaveChanges();
         }
     }
 }
